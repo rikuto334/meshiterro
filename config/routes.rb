5f6970c251cb84455fd5fe9e622be2aa39d9a00c
EC2 Default User <ec2-user@ip-172-31-33-75.ec2.internal>
2022-10-05 05:43:25 +0000

    15syou

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  #get 'users/show'
+  #get 'users/edit'
 get "/post_images" => 'post_images#index'
 get '/post_images/:id' => 'post_images#show', as: 'show'
 delete '/post_images/:id' => 'post_images#destroy', as: 'delete'
+get '/users/:id' => 'users#show', as: 'user_show'
   resource :post_images, only:[:new, :create, :index, :show]
+  resource :users, only: [:show, :edit]
   devise_for :users
   get "/homes/about" => "homes/about", as: "about"
   root to: "homes#top"
