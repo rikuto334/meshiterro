25be624d5c4ce6da92d28c5089805c79ba8275a0
EC2 Default User <ec2-user@ip-172-31-33-75.ec2.internal>
2022-10-05 07:11:29 +0000

    16syou

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
 
   #get 'users/show'
   #get 'users/edit'
-get "/post_images" => 'post_images#index'
-get '/post_images/:id' => 'post_images#show', as: 'show'
+#get "/post_images" => 'post_images#index'
+#get '/post_images/:id' => 'post_images#show', as: 'show'
 delete '/post_images/:id' => 'post_images#destroy', as: 'delete'
-get '/users/:id' => 'users#show', as: 'user_show'
-  resource :post_images, only:[:new, :create, :index, :show]
-  resource :users, only: [:show, :edit]
+#get '/users/:id' => 'users#show', as: 'user_show'
+
   devise_for :users
+   resources :post_images, only:[:new, :create, :index, :show, :delete]
+  resources :users, only: [:show, :edit, :update]
   get "/homes/about" => "homes/about", as: "about"
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
